16f4ead923fd7f6eb5da7fff75b4a57112aa3fbb
Bitwise1057 <mvafeas11@gmail.com>
2025-07-28 21:35:49 -0500

    Added Form Movement
    Mousedown functionality should be available now.

--- src/SN_Gen/About.cs
+++ src/SN_Gen/About.cs
@@ -1,18 +1,48 @@
 using System;
 using System.Net;
 using System.Net.Http;
-using System.Threading.Tasks;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace SN_GEN
 {
     public partial class About : Form
     {
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseCapture();
+
+        // Constants for SendMessage
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
         public About()
         {
             InitializeComponent();
             Version version = Assembly.GetExecutingAssembly().GetName().Version;
             label2.Text = "Current Version: " + version;
+
+            this.MouseDown += About_MouseDown!;
+            panel2.MouseDown += panel2_MouseDown!;
+        }
+
+        private void About_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
         }
 
         private void btnMini_Click(object sender, EventArgs e)

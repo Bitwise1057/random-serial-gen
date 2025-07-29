94d3842dd9d55b917b2158c8e15007890f217a10
Bitwise1057 <mvafeas11@gmail.com>
2025-07-28 21:23:22 -0500

    Update About.cs
    Added rudimentary update check and debug lines.

--- src/SN_Gen/About.cs
+++ src/SN_Gen/About.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.Reflection;
 
 namespace SN_GEN
@@ -22,12 +17,40 @@ namespace SN_GEN
 
         private void btnMini_Click(object sender, EventArgs e)
         {
-			this.WindowState = FormWindowState.Minimized;
-		}
+            this.WindowState = FormWindowState.Minimized;
+        }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Hide();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    string chkdversion = await client.GetStringAsync("https://raw.githubusercontent.com/Bitwise1057/random-serial-gen/main/misc/v"); // Rudimentary update check.
+                    string chkdversion1 = chkdversion.Trim();
+                    string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                    // string version = Application.ProductVersion; // Deprecated Check, leaving for reference.
+                    // MessageBox.Show(chkdversion1); // Debugging
+                    // MessageBox.Show(version); // Debugging
+                    if (chkdversion1 == version)
+                    {
+                        MessageBox.Show("Application is currently up to date", "Update Check");
+                    }
+                    else
+                    {
+                        MessageBox.Show("New version is available. Visit the repository for current version.");
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    MessageBox.Show($"Error Checkign for updates: {ex.Message}", "Update Error");
+                }
+            }
         }
     }
 }
